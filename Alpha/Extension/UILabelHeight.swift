1f4457f32cbee16251183554411b2376c5c5c8dc
Ajeet Victor <ajeet.victor@srmtechsol.com>
2018-12-05 19:43:17 +0530

    Deleted Old Code

--- Alpha/Extension/UILabelHeight.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UILabelHeight.swift
-//  Alpha
-//
-//  Created by Monika Tiwari on 20/04/18.
-//  Copyright © 2018 Stpl. All rights reserved.
-//
-
-import Foundation
--- Pods/Target Support Files/Pods-Alpha/Pods-Alpha-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Alpha : NSObject
-@end
-@implementation PodsDummy_Pods_Alpha
-@end
